a89a51210936b21f63f5d800a9a67ff4ec00c170
Matt Pitner <mattpitner09@gmail.com>
2019-08-13 19:00:13 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,12 +4,20 @@ def countdown(seconds)
   while seconds > 0 
     puts "#{seconds} SECOND(S)!"
     seconds -= 1 
-    sleep(0.5)
   end
   return "HAPPY NEW YEAR!"
  
 end
  
-
+def countdown_with_sleep(seconds)
+  
+    while seconds > 0 
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1 
+    sleep(0.5)
+  end
+  return "HAPPY NEW YEAR!"
+ 
+end
 
 
